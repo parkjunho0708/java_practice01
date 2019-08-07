5cde45978084336fefde6a677c4cfb4c84f3643b
Junho Park <jhpark870708@daum.net>
2019-08-07 13:10:15 +0900

    add comment message

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -1,20 +1,21 @@
 package practice01.prob5;
 
+// 코드정렬 : Ctrl + Shift + F
 public class Prob5 {
 	public static void main(String args[]) {
 		for (int i = 1; i < 100; i++) {
 			int s1 = i / 10;
 			int s2 = i % 10;
 			int clapNumber = 0;
-			
-			if((s2 == 3 || s2 == 6 || s2 == 9))
+
+			if ((s2 == 3 || s2 == 6 || s2 == 9))
 				clapNumber++;
-			if((s1 == 3 || s1 == 6 || s1 == 9))
+			if ((s1 == 3 || s1 == 6 || s1 == 9))
 				clapNumber++;
-			
-			if(clapNumber == 1) {
+
+			if (clapNumber == 1) {
 				System.out.println(i + " 짝");
-			}else if(clapNumber == 2) {
+			} else if (clapNumber == 2) {
 				System.out.println(i + " 짝짝");
 			}
 		}
